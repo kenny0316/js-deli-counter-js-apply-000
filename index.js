7121189a62c451c25b221e3e30ca64bd98e2344d
kehinde ekundayo <kennyekundayo@gmail.com>
2020-06-29 06:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function nowServing(line) {
   if(!line.length) {
     console.log("There is nobody waiting to be served!")
     return "There is nobody waiting to be served!"
-    
+  } else {
+    return "Currently serving " + line.shift('name');
   }
 }
 
